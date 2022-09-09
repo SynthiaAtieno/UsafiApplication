b2a071878685649b68b7d07f766a0b1d54e93b74
SynthiaAtieno <ondengsynthia222@gmail.com>
2022-09-09 20:04:24 +0300

    changed the email

--- app/src/main/java/com/example/usafiapp/Util.java
+++ app/src/main/java/com/example/usafiapp/Util.java
@@ -2,6 +2,6 @@ package com.example.usafiapp;
 
 public class Util {
     //Kindly set your own email and password respectively
-    public  static final String Email = "ondengsynthia222@gmail.com";
+    public  static final String Email = "ondengsynthia2@gmail.com";
     public static final String Password = "1234567890";
 }
